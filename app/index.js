8ca918d223e492ea246bba65a175c6ecb8992237
Zweer <niccolo@olivieriachille.com>
2016-11-30 17:08:54 +0100

    Better comments

--- app/index.js
+++ app/index.js
@@ -34,10 +34,10 @@ _.forEach(models.sequelize.models, (model) => {
 /**
  * Error handlers
  *
- * - 404
- * - 500
+ * - Not Found (catch + forward to error handler)
+ * - Error
  */
-app.use((req, res, next) => { // catch 404 and forward to error handler
+app.use((req, res, next) => {
   const err = new Error(`Not found: ${req.url}`);
 
   err.status = 404;
