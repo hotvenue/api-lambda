15dcd8ff2c30d64121c236ebf2c8fb90761006c5
Zweer <niccolo@olivieriachille.com>
2016-12-08 00:42:47 +0100

    Fixed the parameters

--- things/circleci/deploy.sh
+++ things/circleci/deploy.sh
@@ -7,8 +7,8 @@ aws s3 cp ${LAMBDA_NAME}.zip s3://${S3_BUCKET}
 aws s3 cp swagger.json s3://${S3_BUCKET}/${LAMBDA_NAME}-swagger.json
 
 if aws cloudformation describe-stacks | grep -q ${LAMBDA_NAME} ; then
-  aws cloudformation update-stack --stack-name ${LAMBDA_NAME} --template-body file://./cloudformation.json --capabilities CAPABILITY_IAM --parameters ParameterKey=S3Bucket,ParameterValue=$S3_BUCKET,ParameterKey=SwaggerS3Key,ParameterValue=${LAMBDA_NAME}-swagger.json,ParameterKey=LambdaFunctionS3Key,ParameterValue=${LAMBDA_NAME}.zip
+  aws cloudformation update-stack --stack-name ${LAMBDA_NAME} --template-body file://./things/circleci/cloudformation.json --capabilities CAPABILITY_IAM --parameters file://./things/circleci/cloudformation-params.json
 else
-  aws cloudformation create-stack --stack-name ${LAMBDA_NAME} --template-body file://./cloudformation.json --capabilities CAPABILITY_IAM --parameters ParameterKey=S3Bucket,ParameterValue=$S3_BUCKET,ParameterKey=SwaggerS3Key,ParameterValue=${LAMBDA_NAME}-swagger.json,ParameterKey=LambdaFunctionS3Key,ParameterValue=${LAMBDA_NAME}.zip
+  aws cloudformation create-stack --stack-name ${LAMBDA_NAME} --template-body file://./things/circleci/cloudformation.json --capabilities CAPABILITY_IAM --parameters file://./things/circleci/cloudformation-params.json
 fi
 
--- things/circleci/pre-dependencies.sh
+++ things/circleci/pre-dependencies.sh
@@ -17,3 +17,16 @@ TEMPLATE='{
 }'
 
 printf "$TEMPLATE" ${AWS_ACCESS_KEY_ID} ${AWS_SECRET_ACCESS_KEY} ${PROD_DB_HOSTNAME} ${PROD_DB_USERNAME} ${PROD_DB_PASSWORD} ${PROD_DB_DATABASE} > config/local.json
+
+TEMPLATE_CF_PARAMS='[{
+  "ParameterKey": "S3Bucket",
+  "ParameterValue": "%s"
+}, {
+  "ParameterKey": "SwaggerS3Key",
+  "ParameterValue": "%s"
+}, {
+  "ParameterKey": "LambdaFunctionS3Key",
+  "ParameterValue": "%s"
+}]'
+
+printf "$TEMPLATE_CF_PARAMS" ${S3_BUCKET} "${LAMBDA_NAME}-swagger.json" "${LAMBDA_NAME}.zip" > things/circleci/cloudformation-params.json
